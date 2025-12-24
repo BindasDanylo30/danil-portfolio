85f34028fa0751df6bd72b9802c5110fc839243b
Daniel <danilbindas@gmail.com>
2025-12-24 14:58:17 +0100

    .

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -6,7 +6,7 @@ const Contacts = () => {
       <ul className="content-list">
         <li className="content-list__item">
           <h2 className="title-2">Location</h2>
-          <p>Ivano-Frankivsk, Ukraine</p>
+          <p>Kyev, Ukraine</p>
         </li>
         <li className="content-list__item">
           <h2 className="title-2">Telegram / WhatsApp</h2>
